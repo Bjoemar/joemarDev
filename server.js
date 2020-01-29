171cfa3a0dc8395b5947434c3603bc39603de203
bjoemar <bacleaanjoemar22@gmail.com>
2020-01-29 15:37:38 +0800

    cahnge

--- server.js
+++ server.js
@@ -39,6 +39,11 @@ app.use(express.static('./'));
 //   console.log('Starting server on port 5000');
 // });
 
+// Open Shift Config
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+
 
 server.listen(server_port , server_ip_address , function(){
 	console.log('Listening on' + server_ip_address + ', port' + server_port);	
